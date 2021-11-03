a18690d138a1c420e2b98d5d324e27dfa2304265
Jegor Chomczuk <jegor.chomczuk@gmail.com>
2021-11-03 19:44:08 +0100

    Contact POST method fix

--- src/main/java/com/example/createservice/dto/ContactDTO.java
+++ src/main/java/com/example/createservice/dto/ContactDTO.java
@@ -16,4 +16,14 @@ public class ContactDTO {
     private String companyName;
     private Long salesRepId;
     private Long accountId;
+    private Long id;
+
+    public ContactDTO(String name, String phoneNumber, String email, String companyName, Long salesRepId, Long accountId) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.companyName = companyName;
+        this.salesRepId = salesRepId;
+        this.accountId = accountId;
+    }
 }
